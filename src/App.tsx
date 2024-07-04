ad2134ef36d06aaffe72109718ca8f75677f1904
troach24 <travis.roach24@gmail.com>
2024-07-03 18:55:00 -0600

    query customers

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react';
+import { usePowerSync, useQuery } from "@powersync/react";
+import { PowerSync } from './powersync/powersync';
+import Logger from 'js-logger';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  Logger.useDefaults();
+  usePowerSync();
+  const [customers, setCustomers] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    PowerSync.getAll('SELECT * from customers').then(setCustomers)
+  }, []);
+
+  return <ul>
+    {customers.map(customer => <li key={customer.id}>{customer.name}</li>)}
+  </ul>
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm(), topLevelAwait()],
+  envDir: '..', // Use this dir for env vars, not 'src'.
+  optimizeDeps: {
+    // Don't optimize these packages as they contain web workers and WASM files.
+    // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
+    exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
+    include: [
+      '@powersync/web > event-iterator',
+      '@powersync/web > js-logger',
+      '@powersync/web > lodash/throttle',
+      '@powersync/web > can-ndjson-stream',
+      '@powersync/web > buffer',
+      '@powersync/web > rsocket-core',
+      '@powersync/web > rsocket-websocket-client',
+      '@powersync/web > cross-fetch'
+    ]
+  },
+  worker: {
+    format: 'es',
+    plugins: () => [wasm(), topLevelAwait()]
+  }
 })
